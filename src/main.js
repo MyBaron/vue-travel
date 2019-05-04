0cd5665cd8855139647ed275d10ee46cc2a1677c
lanruqi <249790774@qq.com>
2019-05-04 23:57:03 +0800

    添加轮播图

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/reset.css'
 import './assets/border.css'
 import './assets/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
---
+++ src/views/Home/components/Swiper.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide v-for="(item) in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination"
+        },
+        autoplay: {
+          delay: 1000,
+          stopOnLastSlide: false,
+          disableOnInteraction: true
+        },
+        loop: true
+      },
+      swiperList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/5daa78ddf9a3e3aca32195e079b9545d.jpg_750x200_3aeaa5c2.jpg"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/2680ea76118c1fc8f5d44b4240c20275.jpg_750x200_42900395.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="stylus" scoped>
+
+// 修改子模块的样式
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+.wrapper {
+    // 撑起照片的高度
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 26.7%;
+  background: #eee;
+}
+
+.swiper-img {
+  width: 100%;
+}
+</style>
