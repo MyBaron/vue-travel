7db6c528d5c4c4a5c34e7dd6f8e40005cacddc71
lanruqi <249790774@qq.com>
2019-05-05 20:29:00 +0800

    完成图标

--- src/views/Home/components/Icon.vue
+++ src/views/Home/components/Icon.vue
@@ -1,83 +1,117 @@
-
-
-
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-conntent"
-          src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-        >
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-conntent"
-          src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-        >
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div><div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-conntent"
-          src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-        >
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div><div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-conntent"
-          src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-        >
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div><div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-conntent"
-          src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-        >
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div><div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-conntent"
-          src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-        >
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div><div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-conntent"
-          src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-        >
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div><div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-conntent"
-          src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-        >
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(item, index) in pages" :key="index">
+        <div class="icon" v-for="(item) in item" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-conntent" :src="item.imgUrl">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "HomeIcon",
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination"
+        }
+      },
+      iconList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+          desc: "广州必游"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+          desc: "动植游园"
+        },
+        {
+          id: "004",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png",
+          desc: "自然风光"
+        },
+        {
+          id: "005",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png",
+          desc: "珠江夜游"
+        },
+        {
+          id: "006",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png",
+          desc: "泡温泉"
+        },
+        {
+          id: "007",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+          desc: "一日游"
+        },
+        {
+          id: "008",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/184e261814a5d07a5d3d08cd29cf590d.png",
+          desc: "长隆度假区"
+        },
+        {
+          id: "009",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/c032ae43b15a3dac34b5e07bb0e46850.png",
+          desc: "广州塔"
+        },
+        {
+          id: "010",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png",
+          desc: "广州融创"
+        }
+      ]
+    };
+  },
+  computed: {
+    pages: function() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        //如果为空
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
 @import '~@/assets/veribles.styl';
+
+// 修改子模块的样式
+.icons >>> .swiper-pagination-bullet-active {
+  background: blue;
+}
 
 .icons {
   overflow: hidden;
@@ -118,6 +152,8 @@ export default {};
       line-height: 0.44rem;
       text-align: center;
       color: $darkTextColor;
+      // 超出文字部分用...代替
+      ellipsis();
     }
   }
 }
