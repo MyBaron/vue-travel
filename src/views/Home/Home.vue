1c1b4d36a2b4e2731b816c266616cb55b6dc4e98
lanruqi <249790774@qq.com>
2019-05-06 20:37:14 +0800

    完成axios

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icon></home-icon>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -10,13 +11,27 @@
 import HomeHeader from "./components/Header.vue";
 import HomeSwiper from "./components/Swiper.vue";
 import HomeIcon from "./components/Icon.vue";
+import HomeRecommend from "./components/Recommend.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
   components: {
     HomeHeader: HomeHeader,
     HomeSwiper: HomeSwiper,
-    HomeIcon:HomeIcon
+    HomeIcon: HomeIcon,
+    HomeRecommend: HomeRecommend
+  },
+  methods: {
+    getHomeInfo() {
+      axios.get("/mk/index.json").then(this.getHomeInfoSucc);
+    },
+    getHomeInfoSucc(res) {
+      console.log(res);
+    }
+  },
+  mounted() {
+    this.getHomeInfo();
   }
 };
 </script>
---
+++ src/views/Home/components/Recommend.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <div class="title">
+      <img class="title-img" src="http://img1.qunarzz.com/piao/fusion/1711/89/ebc329f16c55bb02.png">
+      <div>热门推荐</div>
+    </div>
+    <ul>
+      <li class="item" v-for="(item) in itemList" :key="item.id">
+        <img
+          class="item-img"
+          :src="item.imgUrl"
+        >
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">{{item.button}}</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+      return {
+          itemList:[{
+              id:"001",
+              imgUrl:"http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_200x200_ea02ef99.jpg",
+              title:"长隆野生动物世界",
+              desc:"浪漫大连首站，浪漫的海洋公园主题",
+              button:"查看详情"
+          },
+          {
+              id:"002",
+              imgUrl:"http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_200x200_ea02ef99.jpg",
+              title:"长隆野生动物世界",
+              desc:"浪漫大连首站，浪漫的海洋公园主题",
+              button:"查看详情"
+          },
+          {
+              id:"003",
+              imgUrl:"http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_200x200_ea02ef99.jpg",
+              title:"长隆野生动物世界",
+              desc:"浪漫大连首站，浪漫的海洋公园主题",
+              button:"查看详情"
+          }]
+      }
+  },
+};
+</script>
+
+
+<style lang="stylus" scoped>
+@import '~@/assets/veribles.styl';
+.title {
+  margin-top: 0.4rem;
+  line-height: 1.6rem;
+  background-color: #eee;
+  text-indent: 0.4rem; // 第一行缩进多少
+  display: flex;
+}
+
+.title-img {
+  height: 0.93rem;
+  width: 0.93rem;
+  margin-top: 0.3rem;
+  margin-left: 0.3rem;
+  margin-right: 0;
+}
+
+.item {
+  height: 8.625rem;
+  display: flex;
+  overflow: hidden;
+
+  .item-img {
+    width: 6.25rem;
+    height: 6.25rem;
+    padding: 0.625rem;
+  }
+
+  .item-info {
+    flex: 1;
+    padding: 0.625rem;
+    min-width: 0;  //给定元素设置最小宽度
+    
+
+    .item-title {
+      line-height: 1.94rem;
+      font-size: 1.13rem;
+      // 超出文字部分用...代替
+      ellipsis();
+    }
+
+    .item-desc {
+      line-height: 1.75rem;
+      font-size: 0.8rem;
+      color: #ccc;
+      // 超出文字部分用...代替
+      overflow: hidden;
+      ellipsis();
+    }
+
+    .item-button {
+      padding: 0.4rem;
+      background: #ff9300;
+      border-radius: 0.6rem;
+    }
+  }
+}
+</style>
